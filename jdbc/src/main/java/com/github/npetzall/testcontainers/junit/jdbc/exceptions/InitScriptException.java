249a74b125c15bbd5402c5d06aaa3ed50b280ca2
Nils Petzaell <nils.petzall@gmail.com>
2017-06-09 00:22:25 +0200

    QueryStringUsage tests, removed unused constructor

--- jdbc/src/main/java/com/github/npetzall/testcontainers/junit/jdbc/exceptions/InitScriptException.java
+++ jdbc/src/main/java/com/github/npetzall/testcontainers/junit/jdbc/exceptions/InitScriptException.java
@@ -1,11 +1,9 @@
 package com.github.npetzall.testcontainers.junit.jdbc.exceptions;
 
 public class InitScriptException extends RuntimeException {
-    public InitScriptException(String message) {
-        super(message);
-    }
 
     public InitScriptException(String message, Throwable cause) {
         super(message, cause);
     }
+
 }
--- jdbc/src/test/java/com/github/npetzall/testcontainers/junit/jdbc/JdbcContainerRuleTest.java
+++ jdbc/src/test/java/com/github/npetzall/testcontainers/junit/jdbc/JdbcContainerRuleTest.java
@@ -2,10 +2,12 @@ package com.github.npetzall.testcontainers.junit.jdbc;
 
 import com.github.npetzall.testcontainers.junit.jdbc.exceptions.InitScriptException;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.testcontainers.containers.JdbcDatabaseContainer;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
@@ -15,7 +17,6 @@ public class JdbcContainerRuleTest {
     @Test
     public void testInitScriptPathNotFound() {
         JdbcDatabaseContainer container = mock(JdbcDatabaseContainer.class);
-        doNothing().when(container).start();
         JdbcContainerRule<JdbcDatabaseContainer> containerRule = new JdbcContainerRule<>(() -> container)
                 .withInitScript("this_should_not_exist");
         assertThatThrownBy(() -> containerRule.afterStart(container)).isInstanceOf(InitScriptException.class).hasMessageStartingWith("Could not load classpath init script");
@@ -24,10 +25,45 @@ public class JdbcContainerRuleTest {
     @Test
     public void testInitScriptSqlException() throws SQLException {
         JdbcDatabaseContainer container = mock(JdbcDatabaseContainer.class);
-        doNothing().when(container).start();
         when(container.createConnection(anyString())).thenThrow(new SQLException());
         JdbcContainerRule<JdbcDatabaseContainer> containerRule = new JdbcContainerRule<>(() -> container)
                 .withInitScript("org/testcontainers/junit/jdbc/initscripts/mysqlInitScript.sql");
         assertThatThrownBy(() -> containerRule.afterStart(container)).isInstanceOf(InitScriptException.class).hasMessageStartingWith("SQLException");
+    }
+
+    @Test
+    public void  testThatSuppliedQueryStringIsSentToContainerCreateConnectionDuringInitScript() throws SQLException {
+        String expected = "hello";
+        JdbcDatabaseContainer container = mock(JdbcDatabaseContainer.class);
+        ArgumentCaptor<String> queryString = ArgumentCaptor.forClass(String.class);
+        JdbcContainerRule<JdbcDatabaseContainer> containerRule = new JdbcContainerRule<>(() -> container)
+                .withInitScript("org/testcontainers/junit/jdbc/initscripts/mysqlInitScript.sql")
+                .withQueryString(expected);
+        try {
+            containerRule.afterStart(container);
+        } catch (Exception e) {
+            //swallow
+        }
+        verify(container).createConnection(queryString.capture());
+        assertThat(queryString.getValue()).isEqualToIgnoringCase(expected);
+    }
+
+    @Test
+    public void  testThatSuppliedQueryStringIsSentToContainerCreateConnectionDuringInitFunction() throws SQLException {
+        String expected = "hello";
+        JdbcDatabaseContainer container = mock(JdbcDatabaseContainer.class);
+        ArgumentCaptor<String> queryString = ArgumentCaptor.forClass(String.class);
+        JdbcContainerRule<JdbcDatabaseContainer> containerRule = new JdbcContainerRule<>(() -> container)
+                .withQueryString(expected)
+                .withInitFunctions(connection -> {
+                 //nothing
+                });
+        try {
+            containerRule.afterStart(container);
+        } catch (Exception e) {
+            //swallow
+        }
+        verify(container).createConnection(queryString.capture());
+        assertThat(queryString.getValue()).isEqualToIgnoringCase(expected);
     }
 }
